7883c0c7171d2213e00ccce1e0e738be40288fd1
FKnorring <fille.vk@gmail.com>
2023-09-28 16:51:40 +0200

    constants and class defined

--- game.py
+++ game.py
@@ -1,6 +1,59 @@
+from typing import List, Tuple, Union
+from constants import EMPTY, WHITE, WHITE_MIL, WHITE_WIN, BLACK, BLACK_MIL, BLACK_WIN, PLACING, MOVING, FLYING, PLAYING, TIE
+
+def generateBoard(size: int) -> List[List[str]]:
+  return [[EMPTY] * size for _ in range(size)]
+
 
 class Game():
-  pass
+  def __init__(self, size = 8, pieces = 9, difficulty = "EASY") -> None:
+    self.size = size
+    self.board = generateBoard(size)
+    self.hands = { "WHITE": pieces, "BLACK": pieces }
+    self.pieces = { "WHITE": pieces, "BLACK": pieces }
+    self.phase = { "WHITE": PLACING, "BLACK": PLACING }
+    self.state = PLAYING
+    self.turn = "WHITE"
+    self.turns = 0
+    self.ai = AI(difficulty)
+
+  def isValidMove(self, coord1: Tuple[int, int], coord2: Tuple[int, int]) -> bool:
+    pass
+
+  def makeMove(self, coord1: Tuple[int, int], coord2: Tuple[int, int]) -> Union[None, List[List[Tuple[int, int]]]]:
+    pass
+
+  def createMil(self, mil: List[Tuple[int, int]], toRemove: Tuple[int, int]) -> bool:
+    pass
+
+  def isWithinBoard(coord: Tuple[int, int]) -> bool:
+    pass
+
+  def isEmptySquare(coord: Tuple[int, int]) -> bool:
+    pass
+
+  def isAdjacentSquare(coord1: Tuple[int, int], coord2: Tuple[int, int]) -> bool:
+    pass
+
+  def isMoveBreakingMil(coord1: Tuple[int, int], coord2: Tuple[int, int]) -> bool:
+    pass
+
+  def isPieceInMil(coord: Tuple[int, int]) -> bool:
+    pass
+
+  def placeMil(milStart: Tuple[int, int], milEnd: Tuple[int, int]) -> None:
+    pass
+
+  def removeMil(milStart: Tuple[int, int], milEnd: Tuple[int, int]) -> None:
+    pass
+
+  def placePiece(coord: Tuple[int, int]) -> None:
+    pass
+
+  def removePiece(coord: Tuple[int, int]) -> None:
+    pass
+
+  
 
 class AI():
   pass
